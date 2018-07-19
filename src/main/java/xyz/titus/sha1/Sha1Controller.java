2cfd5ebf396b63ed8d1719bc85de3b399326f139
Titus Adi Prasetyo <titusprasetyo@gmail.com>
2018-07-19 12:50:07 +0700

    Pre build to cloud

--- src/main/java/xyz/titus/sha1/Sha1Controller.java
+++ src/main/java/xyz/titus/sha1/Sha1Controller.java
@@ -1,28 +1,36 @@
 package xyz.titus.sha1;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Sha1Controller {
-	/*
-	@RequestMapping(method=RequestMethod.POST, value="/get")
-	@Path("{text}/{key}")
-	public Response getSha1(@PathParam(value="text") String text, @PathParam(value="key") String key) {
-		
-		return Response.status(200).entity("Text|key" + text+"|"+key).build();
-	}
-	*/
 	
-	@RequestMapping(method=RequestMethod.GET, value="/hello")
-	//@Path("{text}/{key}")
-	public String getSha1() {
-		
-		return "Hello";
+	@RequestMapping(method=RequestMethod.POST, value="/sha1/{text}")
+	public String getSha1(@PathVariable(value="text") String text) {
+		String sha1 = "";
+		try {
+			sha1 = sha1(text);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			sha1 = e.getMessage();
+		}
+		return sha1;
+	}
+	
+	private String sha1(String input) throws NoSuchAlgorithmException {
+		MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+		byte[] result = mDigest.digest(input.getBytes());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < result.length; i++) {
+			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		return sb.toString();
 	}
 }
