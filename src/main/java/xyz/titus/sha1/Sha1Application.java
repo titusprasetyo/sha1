1a0f713b4db6067ddd9e7fdacb52b6a5e028f86c
Titus Adi Prasetyo <titusprasetyo@gmail.com>
2018-07-19 11:37:12 +0700

    firts commit

---
+++ src/main/java/xyz/titus/sha1/Sha1Application.java
@@ -0,0 +1,12 @@
+package xyz.titus.sha1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sha1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sha1Application.class, args);
+	}
+}
